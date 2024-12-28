3a7659678a469e04a41a64a386511832376f2164
Rich Stanley II <rich.stanley.ii@gmail.com>
2024-12-28 11:31:12 -0700

    created local IaC resources

--- LRW/main.tf
+++ LRW/main.tf
@@ -1,24 +1,136 @@
-resource "proxmox_vm_qemu" "ICINGA" {
+resource "proxmox_vm_qemu" "HESK" {
     # Node name has to be the same name as within the cluster
     # this might not include the FQDN
-    target_node = "proxmox"
-    desc = "ICINGA"
+    target_node = "proxmox1"
+    desc = "Hesk Helpdesk"
     onboot = true
 
     # The template name to clone this vm from
-    clone = "ubuntucloud"
+    clone = "rocky9cloud"
 
     # Activate QEMU agent for this VM
     agent = 1
 
     os_type = "cloud-init"
     cores = 2
-    sockets = 2
+    sockets = 1
     vcpus = 0
     cpu = "host"
-    memory = 3072
+    memory = 2048
+    name = "HESK"
+    vmid = 301
+
+    # cloudinit = "local-lvm"
+    scsihw   = "virtio-scsi-single" 
+    bootdisk = "scsi0"
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  size = 32
+                }
+            }
+        }
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.1.21/24,gw=192.168.1.1"
+    ciuser = "rich"
+    cipassword = "not2bright"
+    nameserver = "192.168.1.1"
+    sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
+    EOF
+}
+
+resource "proxmox_vm_qemu" "KANBOARD" {
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "proxmox1"
+    desc = "Kanboard"
+    onboot = true
+
+    # The template name to clone this vm from
+    clone = "rocky9cloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 2
+    sockets = 1
+    vcpus = 0
+    cpu = "host"
+    memory = 2048
+    name = "KANBOARD"
+    vmid = 302
+
+    # cloudinit = "local-lvm"
+    scsihw   = "virtio-scsi-single" 
+    bootdisk = "scsi0"
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  size = 32
+                }
+            }
+        }
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.1.23/24,gw=192.168.1.1"
+    ciuser = "rich"
+    cipassword = "not2bright"
+    nameserver = "192.168.1.1"
+    sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
+    EOF
+}
+
+resource "proxmox_vm_qemu" "ICINGA" {
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "proxmox1"
+    desc = "ICINGA"
+    onboot = true
+
+    # The template name to clone this vm from
+    clone = "rocky9cloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 2
+    sockets = 1
+    vcpus = 0
+    cpu = "host"
+    memory = 2048
     name = "ICINGA"
-    vmid = 203
+    vmid = 303
 
     # cloudinit = "local-lvm"
     scsihw   = "virtio-scsi-single" 
@@ -57,24 +169,24 @@ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/
 resource "proxmox_vm_qemu" "GRAFANA" {
     # Node name has to be the same name as within the cluster
     # this might not include the FQDN
-    target_node = "proxmox"
+    target_node = "proxmox1"
     desc = "GRAFANA"
     onboot = true
 
     # The template name to clone this vm from
-    clone = "ubuntucloud"
+    clone = "rocky9cloud"
 
     # Activate QEMU agent for this VM
     agent = 1
 
     os_type = "cloud-init"
-    cores = 4
+    cores = 2
     sockets = 1
     vcpus = 0
     cpu = "host"
-    memory = 3072
+    memory = 2048
     name = "GRAFANA"
-    vmid = 204
+    vmid = 304
 
     # cloudinit = "local-lvm"
     scsihw   = "virtio-scsi-single" 
@@ -113,24 +225,24 @@ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/
 resource "proxmox_vm_qemu" "SYSLOG" {
     # Node name has to be the same name as within the cluster
     # this might not include the FQDN
-    target_node = "proxmox"
+    target_node = "proxmox1"
     desc = "SYSLOG"
     onboot = true
 
     # The template name to clone this vm from
-    clone = "ubuntucloud"
+    clone = "rocky9cloud"
 
     # Activate QEMU agent for this VM
     agent = 1
 
     os_type = "cloud-init"
-    cores = 4
+    cores = 2
     sockets = 1
     vcpus = 0
     cpu = "host"
-    memory = 3072
+    memory = 2048
     name = "SYSLOG"
-    vmid = 205
+    vmid = 305
 
     # cloudinit = "local-lvm"
     scsihw   = "virtio-scsi-single" 
@@ -158,62 +270,6 @@ resource "proxmox_vm_qemu" "SYSLOG" {
     # Setup the ip address using cloud-init.
     # Keep in mind to use the CIDR notation for the ip.
     ipconfig0 = "ip=192.168.1.26/24,gw=192.168.1.1"
-    ciuser = "rich"
-    cipassword = "not2bright"
-    nameserver = "192.168.1.1"
-    sshkeys = <<EOF
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
-    EOF
-}
-
-resource "proxmox_vm_qemu" "DOCKER" {
-    # Node name has to be the same name as within the cluster
-    # this might not include the FQDN
-    target_node = "proxmox"
-    desc = "DOCKER"
-    onboot = true
-
-    # The template name to clone this vm from
-    clone = "ubuntucloud"
-
-    # Activate QEMU agent for this VM
-    agent = 1
-
-    os_type = "cloud-init"
-    cores = 4
-    sockets = 2
-    vcpus = 0
-    cpu = "host"
-    memory = 3072
-    name = "DOCKER"
-    vmid = 206
-
-    # cloudinit = "local-lvm"
-    scsihw   = "virtio-scsi-single" 
-    bootdisk = "scsi0"
-
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                  storage = "local-lvm"
-                  size = 32
-                }
-            }
-        }
-        ide {
-            ide2 {
-                cloudinit {
-                    storage = "local-lvm"
-                }
-            }
-        }
-    }
-
-
-    # Setup the ip address using cloud-init.
-    # Keep in mind to use the CIDR notation for the ip.
-    ipconfig0 = "ip=192.168.1.29/24,gw=192.168.1.1"
     ciuser = "rich"
     cipassword = "not2bright"
     nameserver = "192.168.1.1"
---
+++ local/main.tf
@@ -0,0 +1,446 @@
+resource "proxmox_vm_qemu" "DOCKER" {
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "proxmox"
+    desc = "DOCKER"
+    onboot = true
+
+    # The template name to clone this vm from
+    clone = "ubuntucloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 4
+    sockets = 2
+    vcpus = 0
+    cpu = "host"
+    memory = 10240
+    name = "DOCKER"
+    vmid = 200
+
+    # cloudinit = "local-lvm"
+    scsihw   = "virtio-scsi-single" 
+    bootdisk = "scsi0"
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  size = 32
+                }
+            }
+        }
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.1.29/24,gw=192.168.1.1"
+    ciuser = "rich"
+    cipassword = "not2bright"
+    nameserver = "192.168.1.1"
+    sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
+    EOF
+}
+/*
+resource "proxmox_vm_qemu" "NS1" {
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "proxmox"
+    desc = "DNS & DHCP"
+    onboot = true
+
+    # The template name to clone this vm from
+    clone = "ubuntucloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 4
+    sockets = 2
+    vcpus = 0
+    cpu = "host"
+    memory = 2048
+    name = "NS1"
+    vmid = 510
+
+    # cloudinit = "local-lvm"
+    scsihw   = "virtio-scsi-single" 
+    bootdisk = "scsi0"
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  size = 32
+                }
+            }
+        }
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.1.10/24,gw=192.168.1.1"
+    ciuser = "rich"
+    cipassword = "not2bright"
+    nameserver = "192.168.1.10"
+    sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
+    EOF
+}
+
+resource "proxmox_vm_qemu" "NS2" {
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "proxmox"
+    desc = "DNS & DHCP"
+    onboot = true
+
+    # The template name to clone this vm from
+    clone = "ubuntucloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 4
+    sockets = 2
+    vcpus = 0
+    cpu = "host"
+    memory = 2048
+    name = "NS2"
+    vmid = 511
+
+    # cloudinit = "local-lvm"
+    scsihw   = "virtio-scsi-single" 
+    bootdisk = "scsi0"
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  size = 32
+                }
+            }
+        }
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.1.11/24,gw=192.168.1.1"
+    ciuser = "rich"
+    cipassword = "not2bright"
+    nameserver = "192.168.1.10"
+    sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
+    EOF
+}
+*/
+resource "proxmox_vm_qemu" "HESK" {
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "proxmox1"
+    desc = "HESK"
+    onboot = true
+
+    # The template name to clone this vm from
+    clone = "rocky9cloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 4
+    sockets = 1
+    vcpus = 0
+    cpu = "host"
+    memory = 3072
+    name = "HESK"
+    vmid = 401
+
+    # cloudinit = "local-lvm"
+    scsihw   = "virtio-scsi-single" 
+    bootdisk = "scsi0"
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  size = 32
+                }
+            }
+        }
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.1.21/24,gw=192.168.1.1"
+    ciuser = "rich"
+    cipassword = "not2bright"
+    nameserver = "192.168.1.1"
+    sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
+    EOF
+}
+resource "proxmox_vm_qemu" "ICINGA" {
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "proxmox"
+    desc = "ICINGA"
+    onboot = true
+
+    # The template name to clone this vm from
+    clone = "ubuntucloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 4
+    sockets = 2
+    vcpus = 0
+    cpu = "host"
+    memory = 4096
+    name = "ICINGA"
+    vmid = 403
+
+    # cloudinit = "local-lvm"
+    scsihw   = "virtio-scsi-single" 
+    bootdisk = "scsi0"
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  size = 32
+                }
+            }
+        }
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.1.22/24,gw=192.168.1.1"
+    ciuser = "rich"
+    cipassword = "not2bright"
+    nameserver = "192.168.1.1"
+    sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
+    EOF
+}
+
+resource "proxmox_vm_qemu" "GRAFANA" {
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "proxmox1"
+    desc = "GRAFANA"
+    onboot = true
+
+    # The template name to clone this vm from
+    clone = "rocky9cloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 4
+    sockets = 1
+    vcpus = 0
+    cpu = "host"
+    memory = 3072
+    name = "GRAFANA"
+    vmid = 404
+
+    # cloudinit = "local-lvm"
+    scsihw   = "virtio-scsi-single" 
+    bootdisk = "scsi0"
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  size = 32
+                }
+            }
+        }
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.1.25/24,gw=192.168.1.1"
+    ciuser = "rich"
+    cipassword = "not2bright"
+    nameserver = "192.168.1.1"
+    sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
+    EOF
+}
+
+resource "proxmox_vm_qemu" "SYSLOG" {
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "proxmox1"
+    desc = "SYSLOG"
+    onboot = true
+
+    # The template name to clone this vm from
+    clone = "rocky9cloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 4
+    sockets = 1
+    vcpus = 0
+    cpu = "host"
+    memory = 3072
+    name = "SYSLOG"
+    vmid = 405
+
+    # cloudinit = "local-lvm"
+    scsihw   = "virtio-scsi-single" 
+    bootdisk = "scsi0"
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  size = 32
+                }
+            }
+        }
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.1.26/24,gw=192.168.1.1"
+    ciuser = "rich"
+    cipassword = "not2bright"
+    nameserver = "192.168.1.1"
+    sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
+    EOF
+}
+
+resource "proxmox_vm_qemu" "KANBOARD" {
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "proxmox1"
+    desc = "KANBOARD"
+    onboot = true
+
+    # The template name to clone this vm from
+    clone = "rocky9cloud"
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    os_type = "cloud-init"
+    cores = 4
+    sockets = 1
+    vcpus = 0
+    cpu = "host"
+    memory = 3072
+    name = "KANBOARD"
+    vmid = 402
+
+    # cloudinit = "local-lvm"
+    scsihw   = "virtio-scsi-single" 
+    bootdisk = "scsi0"
+
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                  storage = "local-lvm"
+                  size = 32
+                }
+            }
+        }
+        ide {
+            ide2 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
+        }
+    }
+
+
+    # Setup the ip address using cloud-init.
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=192.168.1.23/24,gw=192.168.1.1"
+    ciuser = "rich"
+    cipassword = "not2bright"
+    nameserver = "192.168.1.1"
+    sshkeys = <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAgpT9X/oGtbEBxfVwDwXA9x2T4WhstuaB/7KzHH+nc/ rich default
+    EOF
+}
