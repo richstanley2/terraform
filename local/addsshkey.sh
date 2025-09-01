74ad79a2266ce20689394630596013297ba53e5b
Rich <rich.stanley.ii@gmail.com>
2025-09-01 10:18:16 -0600

    remove old stuff that is unusable

--- local/addsshkey.sh
+++
@@ -1,7 +0,0 @@
-ssh-copy-id -i ~/.ssh/ansible.pub docker
-ssh-copy-id -i ~/.ssh/ansible.pub icinga
-ssh-copy-id -i ~/.ssh/ansible.pub docker1
-# ssh-copy-id -i ~/.ssh/ansible.pub ns1
-# ssh-copy-id -i ~/.ssh/ansible.pub ns2
-# ssh-copy-id -i ~/.ssh/ansible.pub syslog
-
